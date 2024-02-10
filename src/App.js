b9179ff45f742b7a52252318ec8cade845bbb1ce
JudyHopps007 <153383542+JudyHopps007@users.noreply.github.com>
2024-02-10 09:13:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+    
         <a
           className="App-link"
           href="https://reactjs.org"
